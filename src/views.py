649bd797b4a58859efc99801d4d8b7b6ecbadc4c
Jackie <taekwondo.chan@gmail.com>
2021-02-13 17:44:28 -0800

    fix timestamp check resolving into negative difference, also add missing json error handling

--- src/views.py
+++ src/views.py
@@ -3,6 +3,8 @@ from flask import render_template, request, send_file
 from .scraper import Scraper
 import json
 from datetime import datetime
+import os.path
+from os import path
 
 
 @app.route('/')
@@ -15,15 +17,34 @@ def home():
 @app.route('/<ticker>')
 def get_all(ticker):
 
-    # Unpack json to parse time
-    unpacked_json = open('data.json')
-    data = json.load(unpacked_json)
+    # Detect if json exists, create new json if none found
+    try: 
+        # Unpack json to parse time
+        unpacked_json = open('data.json')
+        data = json.load(unpacked_json)
 
-    format = "%H:%M:%S"
-    old_time = data['timestamp']
-    new_time = (datetime.utcnow()).strftime(format)
-    time_delta = datetime.strptime(
-        new_time, format) - datetime.strptime(old_time, format)
+        format = "%d/%m/%y %H:%M:%S"
+        old_time = data['timestamp']
+        new_time = (datetime.utcnow()).strftime(format)
+        time_delta = datetime.strptime(
+            new_time, format) - datetime.strptime(old_time, format)
+            
+    except:
+        # Run if no JSON found
+        print('No JSON found, creating new JSON with requested index')
+        stock = Scraper(ticker)
+        stock.scrape_page()
+
+        if stock.page_content == False:
+            return 'ERROR - invalid stock index'
+
+        new_data = stock.get_all()
+
+        # new
+        with open('data.json', 'w') as stock_json:
+            json.dump(new_data, stock_json)
+
+        return new_data
 
     # Return old/new scrape depending on time_delta between scrapes
     if time_delta.total_seconds() > 5:
@@ -48,14 +69,3 @@ def get_all(ticker):
         # return old current
         print('Returning old scrape')
         return data
-
-
-@app.route('/<ticker>/name')
-def get_name(ticker):
-    stock = Scraper(ticker)
-    stock.scrape_page()
-
-    if stock.page_content == False:
-        return 'ERROR'
-
-    return stock.get_one('name')
