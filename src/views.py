b80a13f33a48c72109f2c34790ba931ac20adc85
Jackie <taekwondo.chan@gmail.com>
2021-02-18 22:01:26 -0800

    add more logging, fix historical data not appending

--- src/views.py
+++ src/views.py
@@ -6,6 +6,7 @@ from .historical import retrieve_historical
 import json
 from datetime import datetime
 import traceback
+import pprint
 
 
 @app.route('/v1/')
@@ -50,8 +51,13 @@ def get_all(ticker):
             data['historical'] = historical
 
             # Write payload to json
-            new_unpacked_json = open('data.json')
-            data = json.load(new_unpacked_json)
+            with open('data.json', 'w') as new_unpacked_json:
+                json.dump(data, new_unpacked_json)
+
+            # Print json to console for logging
+            with open('data.json') as read_only:
+                json_data = json.load(read_only)
+            print(json.dumps(json_data, indent=1))
 
             return data
 
