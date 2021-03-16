8048632af191e334dfe28ecc741db1aa83a0b8b0
Jackie <taekwondo.chan@gmail.com>
2021-03-15 17:38:45 -0700

    update to use new api

--- src/viewsV2.py
+++ src/viewsV2.py
@@ -1,5 +1,9 @@
-from src import app, historical
+from src import app
 from .robinhood import Robinhood
+import json
+from datetime import datetime
+import traceback
+from flask_api import status
 
 # https://github.com/jmfernandes/robin_stocks
 # http://www.robin-stocks.com/en/latest/robinhood.html#getting-stock-information
@@ -7,11 +11,65 @@ from .robinhood import Robinhood
 
 @app.route('/v2/<ticker>')
 def get_all_v2(ticker):
-    ticker_data = Robinhood.get_ticker(ticker)
+    print(f'{ticker} requested at {datetime.utcnow()}')
 
-    # TODO: error handling
+    # Detect if json exists, create new json if none found
+    try:
+        # Unpack old json to parse time
+        with open('data.json', 'r') as data_json:
+            old_json = json.load(data_json)
 
-    return ticker_data
+        # Determine difference between old/new timestamps
+        format = "%H:%M:%S"
+        old_time = old_json['timestamp']
+        old_price = old_json['current']
+        new_time = (datetime.utcnow()).strftime(format)
+        time_delta = datetime.strptime(
+            new_time, format) - datetime.strptime(old_time, format)
+
+        # Return new scrape if difference in stamps exceeds 5 secs or new index is requested
+        if abs(time_delta.total_seconds()) >= 5 or old_json['symbol'] != ticker.upper():
+
+            # return new current, write new data into json
+            print('Returning new scrape')
+
+            data = Robinhood.get_ticker(ticker)
+
+            # catch bad index
+            if not data:
+                return f'{ticker} not found', status.HTTP_400_BAD_REQUEST
+            
+            # Calculate points change
+            data['points_change'] = round(float(data['current'] - old_price), 2)
+
+            # Write payload to json
+            with open('data.json', 'w') as new_unpacked_json:
+                json.dump(data, new_unpacked_json)
+
+            return data
+
+        else:
+            # return old data if timestamp difference less than 5 secs
+            print('Returning old scrape')
+            return old_json
+
+    except Exception as error:
+        traceback.print_exc()
+
+        # Run if no JSON found
+        print('No JSON found, creating new JSON with requested index')
+
+        data = Robinhood.get_ticker(ticker)
+        print(data)
+
+        if not data:
+            return f'{ticker} not found', status.HTTP_400_BAD_REQUEST
+
+        # Write scrape to new json
+        with open('data.json', 'w') as data_json:
+            json.dump(data, data_json)
+
+        return data
 
 
 @app.route('/v2/<ticker>/historical/<data_range>')
@@ -19,6 +77,7 @@ def get_historical_v2(ticker, data_range):
     historical_data = Robinhood.get_historical(
         ticker, data_range)
 
-    # TODO: error handling
+    if not historical_data:
+        return f'{ticker} not found', status.HTTP_400_BAD_REQUEST
 
     return historical_data
